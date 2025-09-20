bd1b6d0295049c3df32dc28a59278169d54910a6
nassimberrada <Nassim>
2025-09-20 21:12:31 +0100

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdforge',
-    version='0.1.2',
+    version='0.1.3',
     author='nassimberrada',
     description='A Python library for SDF modeling with real-time GLSL rendering and mesh export.',
     long_description=open('README.md').read(),
